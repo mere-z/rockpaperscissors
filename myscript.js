6da13d69bc7af58fcaa05e9053b3bd51fd6abaaf
meredith <shatteredcathedral@gmail.com>
2021-10-20 15:54:37 +1100

    updated script

--- myscript.js
+++ myscript.js
@@ -1,5 +1,34 @@
 function computerPlay() {
     const actions = ["rock", "paper", "scissors"];
-    action = actions[Math.floor(Math.random() * actions.length)]
-    return(action)
+    computerSelection = actions[Math.floor(Math.random() * actions.length)]
+    return(computerSelection)
 }
+
+function playRockPaperScissors(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return("You tie!")
+    }
+    else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "scissors") {
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        } else {
+            return(`You lose! ${playerSelection} loses against ${computerSelection}`)
+        }
+    }
+    else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        } else {
+            return(`You lose! ${playerSelection} loses against ${computerSelection}`)
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        } else {
+            return(`You lose! ${playerSelection} loses against ${computerSelection}`)
+        }
+    }
+}
+
+console.log(playRockPaperScissors("rock", computerPlay()))
